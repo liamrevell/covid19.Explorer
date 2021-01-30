8ad15fddbf669484fe23209e375b6f857f42a276
Liam J. Revell <liamjrevell@gmail.com>
2021-01-30 16:26:17 -0500

    fix version number

--- R/covid.deaths.R
+++ R/covid.deaths.R
@@ -23,7 +23,7 @@ covid.deaths<-function(
 	all.causes=TRUE,
 	cumulative=FALSE,
 	data=list(),
-	xlim=c(60,366+31),
+	xlim=c(32,366+31),
 	bg="transparent",
 	plot=c("standard","smooth","bar"),
 	show=c("raw","per.capita","percent","percent.of.covid.deaths"),
